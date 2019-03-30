c483089093fe179907303b8ee838144552815a09
Alephnaught27 <plume@tutanota.com>
2019-03-29 23:18:05 -0700

    Add a strategy guide for Fubuki's Challenge E-2

--- js/chevent-info.js
+++ js/chevent-info.js
@@ -310,7 +310,42 @@ let MAP_INFO = {
 		<li>LBAS bases available for use will expand as you capture enemy airbases.</li>
 		<li>Capturing transports is vital to success in the transport operation.</li>
 		<li>Thanks again to Minhfongboy for creating difficulty scaling, and to Prinz Eugen-sama for providing boss assets.</li>
-		</ul></i>`,
+		</ul></i>
+		E-2 is a three-phase map: Transporting supplies to the general airbase area, securing control of said airbase, and investigating the anomaly taking place past it.
+		<h3>Progression</h3>
+		<h3>Part 1: Clearing the Transport Phase</h3>
+		You will immediately recognize that something odd is going on with the transport phase with one peek at the gauge - an incredible 4000 TP on Hard difficulty. There is a map mechanic present which gives you 
+		a multliplier for the TP you earn, which is the key to clearing this phase in a timely manner. To take advantage of it, you must use a Transport Combined Fleet and S-rank node F and/or L. Each node S-ranked gives an additional 
+		1.5x multiplier for TP earned during the sortie you S-rank it, meaning you can have up to a 4x TP multiplier in the most optimal case. Routing to these nodes requires that you bring many ships carrying transport items (drums, daihatsu) 
+		and that you keep your fleet light (battleships and carriers aren't treated kindly by the routing).
+		The boss node W is a relatively straightforward installation boss. Utilize your LBAA, WG42, and Type 3 Shells to consistantly secure A-ranks and clear the TP phase with ease!
+		<h3>Part 2: Claiming the Airbase</h3>
+		The second part of the map tasks you with throwing more firepower at the airfield sitting in W and conquer it once and for all. You can use the same TCF that you did during the first phase, but it will be far more 
+		difficult than taking advantage of the Striking Force Fleet option avaiable for this portion. You can bring some battleships and carriers and still take an ideal route, but in the end you still need to bring lighter ships (light cruisers, destroyers) 
+		to secure the best branching. Like in the first phase, good LBAA, WG42, and Type 3 Shells will be very helpful in clearing this boss. Tanks will also come in handy with it comes to defeating installations enroute to the boss. 
+		With an optimized setup, the boss will fall in due time.
+		<h3>Part 3: Investigating the Anomaly</h3>
+		After reclaming the airfield, your task is to use your advantageous position to look at an anomaly that has puzzled command for some time. To conduct your investigation, you must secure total control over the latter 
+		portion of the operational area, that being closest to the anomaly. More specifically, you must accomplish the following:
+		<ul>
+		<li>Hard Difficulty: S-rank nodes Y, Z6; AS+ nodes Y, Z2, Z3, Z4</li>
+		<li>Medium Difficulty: S-rank nodes Y, Z6; AS nodes Z2, Z3, Z4</li>
+		<li>Easy Difficulty: S-rank nodes Y, Z6; AS nodes Z2, Z4</li>
+		<li>Casual Difficulty: S-rank nodes Y, Z6</li>
+		</ul>
+		Using a fleet with many carriers is the key to securing the air raids. Approaching the harbor at node Y is best done using a smaller fleet. When these requirements are met, the boss will be unlocked.
+		<h3>Part 4: Elminating the Anomaly</h3>
+		Now that the anomaly has been identified, proceed to eliminate it! Prior to doing this, it is suggested that you trigger the debuff mechanic present for this boss. To do so:
+		<ul>
+		<li>Hard Difficulty: S-rank nodes W, Y, Z5, Z8</li>
+		<li>Medium Difficulty: S-rank nodes Y, Z5, Z8</li>
+		<li>Easy Difficulty: S-rank nodes Y, Z5; A-rank node Z8</li>
+		<li>Casual Difficulty: S-rank node Y, A-rank node Z8</li>
+		</ul>
+		Use fleets from the earlier parts to effectively defeat the listed nodes. After unlocking the debuff, enemies at the boss receive an armor debuff, and Friend Fleets will arrive at the boss node.
+		For routing to the boss, utilize a Surface Task Force with a limited number of heavy ships and at least one Seaplane Tender to secure the most ideal route. LHA can shorten the route taken, but not quite to the 
+		extent that AV can. The boss node is packed full of installation-types that are weak to Type 3 Shells, bringing them is essentially mandatory for clearing. PT Imps are also present at the boss node; equipment that is 
+		effective against them should be brought with you to counter them. The use of special attacks is advised, as the boss can prove difficult even with the debuff active.`,
 		3:`<h2>E-3 Strategy Guide</h2>
 		<i>The area around Vela Gulf has been attacked and conquered by a strong abyssal force, one of the most imposing ones ever witnessed.<br>
 		Reclaim the airbase with an initial search party, then use it in conjunction with the main body of the fleet to converge on and destroy the enemy task force!<br>
